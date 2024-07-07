26028b069881bd2571adf5e38140909b142cc46d
hln5 <harrynguyen019@gmail.com>
2024-07-06 18:08:41 -0700

    refactor lox function code into different file

---
+++ src/Function.rs
@@ -0,0 +1,100 @@
+use crate::{
+    environment::{EnvRef, Environment},
+    interpreter::Interpreter,
+    scanner::Token,
+    stmt::Stmt,
+    value::Value,
+    Exception,
+};
+
+pub trait Callable {
+    fn arity(&self) -> usize;
+    fn call(&self, interpreter: &mut Interpreter, args: Vec<Value>) -> Result<Value, Exception>;
+    fn check_arity(&self, args_len: usize, current_token: &Token) -> Result<(), Exception> {
+        if args_len > self.arity() {
+            return Exception::runtime_error(
+                current_token.clone(),
+                format!("Expected {} arguments but got {}.", self.arity(), args_len),
+            );
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct NativeFunction {
+    pub arity: usize,
+    pub callable: fn(&mut Interpreter, Vec<Value>) -> Value,
+}
+impl Callable for NativeFunction {
+    fn arity(&self) -> usize {
+        self.arity
+    }
+
+    fn call(&self, interpreter: &mut Interpreter, args: Vec<Value>) -> Result<Value, Exception> {
+        Ok((self.callable)(interpreter, args))
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Function {
+    declaration: Stmt,
+    closure: EnvRef,
+}
+
+impl Function {
+    pub fn new(declaration: Stmt, closure: EnvRef) -> Function {
+        match &declaration {
+            Stmt::Function {
+                name: _,
+                params: _,
+                body: _,
+            } => Function {
+                declaration,
+                closure,
+            },
+            _ => panic!("Function was not passed a function declaration!"),
+        }
+    }
+}
+
+impl Callable for Function {
+    fn arity(&self) -> usize {
+        if let Stmt::Function {
+            name: _,
+            params,
+            body: _,
+        } = &self.declaration
+        {
+            return params.len();
+        }
+        panic!("Function was not passed a function declaration!");
+    }
+
+    fn call(&self, interpreter: &mut Interpreter, args: Vec<Value>) -> Result<Value, Exception> {
+        let environment = Environment::new_local(&self.closure);
+
+        if let Stmt::Function {
+            name: _,
+            params,
+            body,
+        } = &self.declaration
+        {
+            for (i, param) in params.iter().enumerate() {
+                environment
+                    .borrow_mut()
+                    .define(param.lexeme.clone(), args.get(i).unwrap().clone())
+            }
+
+            if let Err(exception) = interpreter.execute_block(body, environment) {
+                match exception {
+                    Exception::RuntimeError(e) => return Err(Exception::RuntimeError(e)),
+                    Exception::Return(e) => return Ok(e),
+                }
+            }
+        }
+
+        Ok(Value::Nil)
+    }
+}
