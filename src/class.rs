a20f8431889e22a72875c53a5c8203ed1366fa56
Harry Nguyen <harry.nguyen@motorolasolutions.com>
2024-07-10 15:23:00 -0700

    setup class instances

--- src/class.rs
+++ src/class.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
 
+use crate::{function::Callable, interpreter::Interpreter, value::Value, Exception};
+
 #[derive(Clone, Debug)]
 pub struct Class {
     name: String,
@@ -11,8 +13,33 @@ impl Class {
     }
 }
 
+// class constructor
+impl Callable for Class {
+    fn arity(&self) -> usize {
+        0
+    }
+
+    fn call(&self, interpreter: &mut Interpreter, args: Vec<Value>) -> Result<Value, Exception> {
+        let instance = ClassInstance {
+            class: self.clone(),
+        };
+        Ok(Value::ClassInstance(instance))
+    }
+}
+
 impl Display for Class {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.name)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ClassInstance {
+    class: Class,
+}
+
+impl Display for ClassInstance {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} instance", self.class)
     }
 }
