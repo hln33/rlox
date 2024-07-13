ead69eb89c301b3d22747648dd82672481b3af66
hln5 <harrynguyen019@gmail.com>
2024-07-12 17:11:27 -0700

    small cleanup

--- src/function.rs
+++ src/function.rs
@@ -87,15 +87,16 @@ impl Callable for Function {
             }
 
             if let Err(exception) = interpreter.execute_block(body, environment) {
-                match exception {
-                    Exception::RuntimeError(e) => return Err(Exception::RuntimeError(e)),
+                return match exception {
+                    Exception::RuntimeError(e) => Err(Exception::RuntimeError(e)),
                     Exception::Return(value) => {
                         if self.is_initializer {
-                            return self.closure.borrow().get_at(0, "this");
+                            self.closure.borrow().get_at(0, "this")
+                        } else {
+                            Ok(value)
                         }
-                        return Ok(value);
                     }
-                }
+                };
             }
         }
 
